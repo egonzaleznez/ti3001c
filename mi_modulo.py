523d86dd95845fe443b2e313f297efcab321e272
egonzaleznez <160289892+egonzaleznez@users.noreply.github.com>
2025-08-19 11:08:17 -0600

    cambios

---
+++ mi_modulo.py
@@ -0,0 +1,22 @@
+import numpy as np # to use numpy arrays instead of lists
+
+
+# --------------------
+def saludoUno():
+    """
+    Say Hi
+    """
+
+    print("Hi")
+
+
+# --------------------
+def saludoDos():
+    """
+    Say Hi
+    """
+
+    text = "Hello"
+    print(text)
+
+    return text
